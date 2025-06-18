7c3cf6053458cda6c360fbeba819967962bc417f
AsaTyr <43162495+AsaTyr2018@users.noreply.github.com>
2025-06-18 20:44:46 +0200

    Fix manager script and update entry script repo URL

--- fooocus_manager.sh
+++ fooocus_manager.sh
@@ -13,16 +13,24 @@ fi
 install_app() {
     $SUDO mkdir -p "$INSTALL_DIR"
     $SUDO rsync -a "$REPO_SOURCE/" "$INSTALL_DIR/"
+
     if [ ! -d "$INSTALL_DIR/venv" ]; then
         $SUDO python3 -m venv "$INSTALL_DIR/venv"
     fi
+
     $SUDO "$INSTALL_DIR/venv/bin/pip" install --upgrade pip
     $SUDO "$INSTALL_DIR/venv/bin/pip" install -r "$INSTALL_DIR/requirements_versions.txt"
+
+    if git -C "$INSTALL_DIR" remote | grep -q '^origin$'; then
+        $SUDO git -C "$INSTALL_DIR" remote set-url origin https://github.com/AsaTyr2018/Fooocus_revamp
+    else
+        $SUDO git -C "$INSTALL_DIR" remote add origin https://github.com/AsaTyr2018/Fooocus_revamp
+    fi
 }
 
 update_app() {
     cd "$INSTALL_DIR"
-    $SUDO git pull
+    $SUDO git pull origin "$(git rev-parse --abbrev-ref HEAD)"
     $SUDO "$INSTALL_DIR/venv/bin/pip" install -r requirements_versions.txt
 }
 
