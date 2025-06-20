5bd358c2c9d342c45fc6339b38369ae8fdbc24f2
AsaTyr <43162495+AsaTyr2018@users.noreply.github.com>
2025-06-20 17:12:15 +0200

    Add custom gradio theme and disable analytics

---
+++ modules/theme.py
@@ -0,0 +1,8 @@
+import gradio as gr
+from gradio.themes.utils import colors
+
+# Custom theme tuned for Fooocus Revamp
+FooocusTheme = gr.themes.Soft(
+    primary_hue=colors.blue,
+    secondary_hue=colors.indigo,
+)
